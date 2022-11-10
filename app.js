44f816479f52e28c64513fa01c79212b604fbccf
Faizan Siddiqui <faizansiddiquirmr@gmail.com>
2022-11-10 19:38:14 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+let buttons =  document.querySelectorAll('.btn');
+let screen = document.querySelector('input'); 
+buttons.forEach((button) => {
+  button.addEventListener('click', (e)=>{
+    let value = e.target.innerHTML; 
+    if(e.target == '='){
+      screen.value += value
+    }
+    else{
+      screen.value += value;
+    }
+  })
+});
+
+let clear = document.querySelector('.com1');
+clear.addEventListener('click', (e)=>{
+  screen.value = "";
+})
+
+let equal = document.querySelector('.com-1');
+equal.addEventListener('click', (e)=>{
+  if(screen.value === ""){
+    screen.value = "0";
+  }
+  else{
+    try{
+      let ans = eval(screen.value);
+      screen.value = ans;
+    }catch{
+      screen.value = "Error"
+    }   
+  }
+})
+
+let del = document.querySelector('.com2');
+del.addEventListener('click', (e) =>{
+  screen.value = screen.value.slice(0, -1)
+})
